642547b71deed233b0b0a0aaeb90fffd053c2cb6
Christopher Danan <chrisddan@gmail.com>
2015-04-13 17:56:04 -0700

    Create DOM elements that allow the user to input a URL. Add code in server to receive entered URL and determine if it is a "long" URL or a "short" URL.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,23 @@
 var main = function(){
 	"use strict";
 
-	console.log("Hello Vane!");
+	//DOM elements for the area where user can input URLs.
+	var $inputURL = $("<input>").attr({"id": "inputURLText", "name": "inputURL"}),
+		$URLSubmitBtn = $("<button>").text("Submit").attr("id", "inputURLBtn"),
+		$tagLabel = $("<label>").text("Enter URL here").attr("for", "inputURL");
+
+	$("#inputURLDiv").append($tagLabel);
+	$("#inputURLDiv").append($inputURL);
+	$("#inputURLDiv").append($URLSubmitBtn);
+
+	$URLSubmitBtn.on("click", function(){
+		var inputURL = $inputURL.val();
+		$inputURL.val("");
+
+		$.post("/shorter", {url: inputURL}, function(res){
+			console.log("Posted to the server and got back a response.");
+		});
+	});
 };
 
 $(document).ready(main);
--- server.js
+++ server.js
@@ -31,3 +31,23 @@ router.route("/")
 	.get(function(req, res){
 		res.render("index", {title: "URL Shortener"});
 	});
+
+router.route("/shorter")
+	.post(function(req, res){
+		console.log("Received POST from client.");
+		console.log(req.body);
+
+		var originalURL,  //The original URL entered by the user.
+			base,  //The base part of the URL, which in this program should be "http://localhost:<port number>".
+			path;  //The path part of the URL, which will usually be the part we will shorten.
+
+		originalURL = req.body.url;
+		base = "http://localhost:" + port;
+
+		//Reference for checking if string contains substring: http://stackoverflow.com/questions/1789945/how-can-i-check-if-one-string-contains-another-substring
+		if(originalURL.indexOf(base) > -1){  //Entered URL starts with base, so assume user wants a shortened URL.
+			console.log("User entered long URL");
+		} else{  //Entered URL does not start with base, so assume user entered a shortened URL and wants the long URL.
+			console.log("User entered a short URL");
+		}
+	});
