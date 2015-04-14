7a74ff6212784535e9296da8727a41769863b9d3
Christopher Danan <chrisddan@gmail.com>
2015-04-13 20:43:05 -0700

    Handle creating a new key for short URLs.

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var express = require("express"),
 	router = express.Router(),
 	port = 3000,
 	app,
-	redisClient;
+	redisClient,
+	initialKey = 10 * Math.pow(36, 3);  //Initial key used for Redis.
 
 app = express();
 redisClient = redis.createClient();
@@ -24,6 +25,34 @@ http.createServer(app).listen(port);
 console.log("Server listening on port " + port);
 
 //Functions
+function getNextKey(originalURL, base){
+
+	var newKey;
+
+	redisClient.setnx("next", initialKey, function(err, data){  //Set next to initialKey if next does not exist in the db.
+
+		var increaseValue = Math.floor(Math.random() * 10 + 1);  //Get the value we will increase next by.
+
+		redisClient.incrby("next", increaseValue);  //Increase next by increaseValue, giving a unique key we can give as a shortened URL.
+
+		redisClient.get("next", function(err, nextCount){
+			if(err !== null){
+				console.log("ERROR: " + err);
+				return;
+			}
+
+			newKey = parseInt(nextCount, 10);  //Get the value of next in the database.
+			newKey = newKey.toString(36);  //Convert the value of next to a base36 string.
+
+			redisClient.setnx("short:" + base + newKey, originalURL);
+			redisClient.set("long:" + originalURL, base + "/" + newKey);
+		});
+	});
+}
+
+function giveShortURL(){
+	
+}
 
 //Routes
 //Route for homepage.
@@ -39,7 +68,8 @@ router.route("/shorter")
 
 		var originalURL,  //The original URL entered by the user.
 			base,  //The base part of the URL, which in this program should be "http://localhost:<port number>".
-			path;  //The path part of the URL, which will usually be the part we will shorten.
+			path,  //The path part of the URL, which will usually be the part we will shorten.
+			key;  //The key used to store a new shortened URL in the database.
 
 		originalURL = req.body.url;
 		base = "http://localhost:" + port;
@@ -47,6 +77,21 @@ router.route("/shorter")
 		//Reference for checking if string contains substring: http://stackoverflow.com/questions/1789945/how-can-i-check-if-one-string-contains-another-substring
 		if(originalURL.indexOf(base) > -1){  //Entered URL starts with base, so assume user wants a shortened URL.
 			console.log("User entered long URL");
+			redisClient.exists("long:" + originalURL, function(err, result){
+				if(err !== null){
+					console.log("ERROR: " + err);
+					return;
+				}
+
+				if(result === 0){  //The long URL does not exist in the database.  Create a short URL.
+					console.log("The URL entered does not exist in the database");
+					getNextKey(originalURL, base);
+					giveShortURL();
+				} else if(result === 1){  //The long URL does exist in the database.  Fetch the short URL.
+					console.log("The URL entered exists in the database.");
+					giveShortURL();
+				}
+			});
 		} else{  //Entered URL does not start with base, so assume user entered a shortened URL and wants the long URL.
 			console.log("User entered a short URL");
 		}
